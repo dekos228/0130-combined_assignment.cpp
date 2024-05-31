7b8f7a28bea18c13516e16413eb35908201ecf57
dekos228 <dekos589191@gmail.com>
2024-05-31 20:28:51 +0200

    Create 0130-combined_assignment.cpp

---
+++ 0130-combined_assignment.cpp
@@ -0,0 +1,18 @@
+﻿// combined_assignment.cpp
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+  using namespace std;
+
+  string line1, line2; // Сразу две переменные, пустые строки.
+  getline(cin, line1);
+  getline(cin, line2);
+
+  // То же, что line1 = line1 + line2, но без создания временного объекта.
+  line1 += line2; // Заменить первую строку на результат "склеивания".
+  cout << line1 << endl;
+  return EXIT_SUCCESS;
+}
